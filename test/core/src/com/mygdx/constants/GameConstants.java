b3d8b7dd8b3523e87db0e8382d8153642572251f
manilonis <35714442+manilonis@users.noreply.github.com>
2018-05-29 14:31:08 -0400

    First Steps of Game Logic
    A player can now moves every frame. Game Constants class added

---
+++ test/core/src/com/mygdx/constants/GameConstants.java
@@ -0,0 +1,11 @@
+/**
+ * Michael E Anilonis
+ * May 29, 2018
+ */
+package com.mygdx.constants;
+
+
+public class GameConstants {
+	public static int width = 1300;
+	public static int height = 970;
+}
--- test/core/src/com/mygdx/game/MyGame.java
+++ test/core/src/com/mygdx/game/MyGame.java
@@ -4,25 +4,33 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGame extends Game {
+	
+	//only have one SpriteBatch
 	SpriteBatch batch;
 	Texture img;
+	Sprite player;
 	
 	@Override
 	public void create () {
 		System.out.println("Create was Called");
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("vine.jpg");
+		player = new Sprite(new Texture("firefighter.jpg"));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		player.setPosition(player.getX() +1 , player.getY());
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(player,player.getX(), player.getY());
 		batch.end();
 	}
 	
